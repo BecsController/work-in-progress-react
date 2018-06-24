854166f9bd366fd85475eb77fe17453dce3ef78b
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-06-24 19:01:23 +1200

    page changes

--- client/components/StoryPageOne.jsx
+++ client/components/StoryPageOne.jsx
@@ -4,39 +4,59 @@ import {Link} from 'react-router-dom'
 class StoriesPageOne extends React.Component {
   constructor (props){
     super (props)
+
     this.state = {
       pages: [{
+        id: 1,
         title: 'Beginnings',
-        population: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ8TDAZZuow6x2IH-Pnhs8prAK1iolyqM8WRKKhpKv7yIDrzee",
+        background: "/images/backgrounds/house.jpeg",
+        population: '/images/parents.jpeg',
         emotionOne: 'I am feeling Anxious',
         emotionTwo: 'I am feeling Excited',
         emotionThree: 'I am feeling Scared',
-        pageText: 'Welcome to your first day of school! Your teacher comes over to introduce herself. Hi, I\'m Miss Davis, what\'s your name? She holds out her hand.'
+        pageText: "It\'s the first day of school. It\'s time to get ready to go.\n Your parents say, 'Do you remember why it\'s important to go to school?'",
+        optionOne: '',
+        optionTwo: '',
+      },{
+        id: 2,
+        title: 'The Classroom',
+        background: "/images/backgrounds/inside-a-kindergarten-classroom-background.jpg",
+        population: "/images/teacher.jpeg",
+        emotionOne: 'I am feeling Anxious',
+        emotionTwo: 'I am feeling Excited',
+        emotionThree: 'I am feeling Scared',
+        pageText: 'In the classroom, your teacher comes over to introduce herself. \n Hi, I\'m Miss Davis, what\'s your name? \n She holds out her hand.',
+        optionOne: '',
+        optionTwo: ''
       }]
     }
   }
 
   render() {
+    let pageTracker = 0;
     const resizeMode = 'center';
   return (
     <div className="columns is-two-thirds">
         <div className="box column is-10 is-offset-1 page-template">
-              <h1 className="has-text-centered is-size-2">{this.state.pages[0].title}</h1>
-                <img style={{flex: 1, resizeMode, position:'relative', zIndex:1,}} className="school-bg-img" src="/images/backgrounds/inside-a-kindergarten-classroom-background.jpg"/>
+              <h1 className="has-text-centered is-size-2">{this.state.pages[pageTracker].title}</h1>
+                <img style={{flex: 1, resizeMode, position:'relative', zIndex:1,}} className="school-bg-img" src={this.state.pages[pageTracker].background}/>
                  <div className="container is-full-height ">
-                <img className="teacher" src={this.state.pages[0].population} alt="teacher"/>
-                  <button id="anxious" className="is-warning is-rounded button is-medium">{this.state.pages[0].emotionOne}</button>
-                  <button id="excited" className="is-warning button is-rounded is-medium is-pulled-right">{this.state.pages[0].emotionTwo}</button>
-                  <button id="scared" className="is-warning button is-rounded is-medium is-pulled-left">{this.state.pages[0].emotionThree}</button>
+                <img className="teacher" src={this.state.pages[pageTracker].population} alt="teacher"/>
+                  <button id="anxious" className="is-hidden is-warning is-rounded button is-medium">{this.state.pages[pageTracker].emotionOne}</button>
+                  <button id="excited" className="is-hidden is-warning button is-rounded is-medium is-pulled-right">{this.state.pages[pageTracker].emotionTwo}</button>
+                  <button id="scared" className="is-hidden is-warning button is-rounded is-medium is-pulled-left">{this.state.pages[pageTracker].emotionThree}</button>
                   <div className="box column is-10 textarea">
                     <p className="has-text-centered is-size-4">
-                      {this.state.pages[0].pageText}
+                      {this.state.pages[pageTracker].pageText}
                       <Link className="button is-medium is-pulled-right" to={`/story/Off%20To%20School/2`}>
-                        Next &nbsp;
+                        Click to answer &nbsp;
                         <span className="icon">
                           <i className="far fa-arrow-alt-circle-right"></i>
                         </span>
                       </Link>
+                    </p>
+                    <p className="has-text-centered is-size-4">
+                      {this.state.pages[pageTracker].optionOne}
                     </p>
                   </div>
           </div>
